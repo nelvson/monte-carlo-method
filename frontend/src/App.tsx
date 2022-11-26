f76157e8ba5d408cdcb6a68089df0c69627cf005
nelvson <nelvson1337@gmail.com>
2022-11-26 08:37:27 +0700

    add <Footer />

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -90,6 +90,89 @@ function Axis(props: { data: Array<AxisXY> }) {
   return <svg ref={ref} />;
 }
 
+const HowDoesItWork = () => {
+  return (
+    <div className="how-does-it-work">
+      <h3>How does this work?</h3>
+      <div>
+        <figure>
+          <img
+            alt="wiki"
+            height={200}
+            width={200}
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Pi_30K.gif/440px-Pi_30K.gif"
+          />
+          <figcaption>Monte Carlo method for estimating Pi</figcaption>
+        </figure>
+
+        <div>
+          <p>
+            Given a square with a squadrant inside of it, uniformly scatter a
+            given number of points <i>n</i> over the square.
+          </p>
+          <p>
+            The ratio of the inside-count and the total-sample-count is an
+            estimate of the ratio of the two areas times 4 equals to estimate π.
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <p>
+            By the{" "}
+            <a href="https://en.wikipedia.org/wiki/Law_of_large_numbers">
+              law of large numbers (LLN),
+            </a>{" "}
+            the larger the number of <i>n</i>, the closer the pi estimation to
+            actual value of π.
+          </p>
+          <p>
+            Try different value of <i>n</i>, and try it by yourself!
+          </p>
+        </div>
+        <figure>
+          <img
+            alt="Law of large numbers"
+            height={200}
+            width={300}
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Lawoflargenumbers.svg/1920px-Lawoflargenumbers.svg.png"
+          />
+          <figcaption>LLN for rolls of a single dice</figcaption>
+        </figure>
+      </div>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="citations">
+        <h4>Citations & Sources</h4>
+        <a href="https://en.wikipedia.org/wiki/Monte_Carlo_method">
+          <p>Monte Carlo Method - Wikipedia</p>
+        </a>
+        <a href="https://en.wikipedia.org/wiki/Law_of_large_numbers">
+          <p>Law of Large Numbers - Wikipedia</p>
+        </a>
+      </div>
+      <div className="readmore">
+        <h4>Read more about Monte Carlo Method!</h4>
+        <a href="https://www.jstor.org/stable/2686489">
+          <p>
+            Determining Sample Sizes for Monte Carlo Integration. Neal, David.
+            1993
+          </p>
+        </a>
+        <a href="https://blogs.sas.com/content/iml/2016/03/14/monte-carlo-estimates-of-pi.html">
+          <p>Monte Carlo estimates of pi and an important statistical lesson | Blog SAS</p>
+        </a>
+      </div>
+    </div>
+  );
+};
+
 function App() {
   let [numberOfPoints, setNumberOfPoints] = useState(0);
   let [points, setPoints] = useState<Array<AxisXY>>([]);
@@ -140,7 +223,9 @@ function App() {
             fetch
           </button>
 
-          <text>n: <text id="totalPointsNode" /> </text>
+          <text>
+            n: <text id="totalPointsNode" />{" "}
+          </text>
           <text>
             number of points inside circle: <text id="pointsInside" />
           </text>
@@ -150,15 +235,10 @@ function App() {
         </div>
       </div>
 
-      <div className="how-does-it-work">
-        <h3>
-        How does this work?
-        </h3>
-        <p>Given a square with a squadrant inside of it, uniformly scatter a given number of points <i>n</i> over the square.</p>
-        <p>The ratio of the inside-count and the total-sample-count is an estimate of the ratio of the two areas times 4 equals to estimate π.</p>
-        <p>By the law of large numbers, the larger the number of <i>n</i>, the closer the pi estimation to actual value of π.</p>
+      <hr color="#4153C9" style={{ height: 5 }} />
+      <HowDoesItWork />
 
-      </div>
+      <Footer />
     </div>
   );
 }
